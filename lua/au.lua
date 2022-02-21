672f8e0b5442c8eef8901ac127714d9bdfa933ac
Aetf <aetf@unlimited-code.works>
2022-02-20 22:35:34 -0500

    support once and nested in autocommand

--- lua/au.lua
+++ lua/au.lua
@@ -44,23 +44,23 @@ local function autocmd(this, event, spec)
     local pattern = is_table and spec[1] or '*'
     pattern = type(pattern) == 'table' and table.concat(pattern, ',') or pattern
 
+    -- once if present, translates to ++once
+    local once = is_table and spec.once or false
+
+    -- nested if present, translates to ++nested
+    local nested = is_table and spec.nested or false
+
     -- action can be a function or string
     local action = is_table and spec[2] or spec
     if type(action) == 'function' then
-        action = this.set(action)
+        action = this.set(action, once)
     end
 
     -- event can be a table or string
     local e = type(event) == 'table' and table.concat(event, ',') or event
 
-    -- once if present, translates to ++once
-    local once = is_table and spec.once or false
     once = once and '++once' or ''
-
-    -- nested if present, translates to ++nested
-    local nested = is_table and spec.nested or false
     nested = nested and '++nested' or ''
-
     cmd(table.concat({'autocmd', e, pattern, once, nested, action}, ' '))
 end
 
@@ -74,14 +74,21 @@ local X = setmetatable({}, {
     __call = autocmd,
 })
 
-function S.exec(id)
+---@param id string
+---@param once boolean
+function S.exec(id, once)
     S.__au[id]()
+    if once then
+        S.__au[id] = nil
+    end
 end
 
-function S.set(fn)
+---@param fn fun()
+---@param once boolean
+function S.set(fn, once)
     local id = string.format('%p', fn)
     S.__au[id] = fn
-    return string.format('lua require("au").exec("%s")', id)
+    return string.format('lua require("au").exec("%s", %s)', id, once)
 end
 
 function S.group(grp, cmds)
@@ -91,8 +98,8 @@ function S.group(grp, cmds)
         cmds(X)
     else
         for _, au in ipairs(cmds) do
-            local ptn = table.remove(au, 1)
-            autocmd(S, ptn, au)
+            local evt = table.remove(au, 1)
+            autocmd(S, evt, au)
         end
     end
     cmd('augroup END')
