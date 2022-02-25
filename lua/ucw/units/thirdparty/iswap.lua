dac3885b8e1c0dafce148ed826727b0e2d787c33
Aetf <aetf@unlimited-code.works>
2022-02-24 19:15:36 -0500

    Add iswap

---
+++ lua/ucw/units/thirdparty/iswap.lua
@@ -0,0 +1,19 @@
+---@type nvimd.Unit
+local M = {}
+
+M.url = 'mizlan/iswap.nvim'
+M.description = 'Swap function arguments'
+
+M.activation = {
+  cmd = {
+    'ISwap',
+    'ISwapWith',
+  }
+}
+
+function M.config()
+  require('iswap').setup {
+  }
+end
+
+return M
