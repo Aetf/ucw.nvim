8d24d8b93405362c501e846ab1d54e0c62f27648
Aetf <aetf@unlimited-code.works>
2022-04-21 20:37:49 -0400

    Some octo keybindings

--- lua/ucw/units/thirdparty/octo.lua
+++ lua/ucw/units/thirdparty/octo.lua
@@ -18,6 +18,17 @@ M.activation = {
 function M.config ()
   require('octo').setup {
   }
+  local wk = require('which-key')
+  wk.register {
+    ['<leader>g'] = {
+      o = {
+        name = "+octo (GitHub)",
+        o = { [[<cmd>Octo actions<cr>]], "Pick an action" },
+        i = { [[<cmd>Octo issue search<cr>]], "Search issues" },
+        p = { [[<cmd>Octo pr search<cr>]], "Search issues" },
+      }
+    }
+  }
 end
 
 return M
