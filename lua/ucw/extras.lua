575cb6923c2f704c48cae7d087f9b37c07460723
Aetf <aetf@unlimited-code.works>
2022-04-24 01:55:15 -0400

    Jump back to last accessed window on close
    fix #9

--- lua/ucw/extras.lua
+++ lua/ucw/extras.lua
@@ -21,3 +21,28 @@ au.group('RestoreLastCursor', {
   }
 })
 
+-- return to last accessed window when closing current one
+au.group('RestoreLastWindow', {
+  {
+    { 'VimEnter', 'WinEnter' }, '*',
+    function()
+      -- Exclude floating windows
+      if '' ~= vim.api.nvim_win_get_config(0).relative then return end
+      -- Record the window we jump from (previous) and to (current)
+      if nil == vim.t.winid_rec then
+        vim.t.winid_rec = { prev = vim.fn.win_getid(), current = vim.fn.win_getid() }
+      else
+        vim.t.winid_rec = { prev = vim.t.winid_rec.current, current = vim.fn.win_getid() }
+      end
+
+      -- Loop through all windows to check if the previous one has been closed
+      for winnr=1,vim.fn.winnr('$') do
+        if vim.fn.win_getid(winnr) == vim.t.winid_rec.prev then
+          return        -- Return if previous window is not closed
+        end
+      end
+
+      vim.cmd [[ wincmd p ]]
+    end
+  }
+})
