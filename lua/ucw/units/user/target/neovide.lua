479a27730803a3353178e2ff9f35123850f70d78
Aetf <aetf@unlimited-code.works>
2022-04-29 16:42:35 -0400

    Use Comic Code as gui font

--- lua/ucw/units/user/target/neovide.lua
+++ lua/ucw/units/user/target/neovide.lua
@@ -9,12 +9,24 @@ M.activation = {
 }
 
 function M.config()
-  vim.opt.guifont = 'Hack Nerd Font Mono:h13'
+  -- vim.opt.guifont = 'Comic Code Ligatures:h12'
+  vim.opt.guifont = 'ComicCodeLigatures Nerd Font Mono:h12'
 
   vim.g.neovide_remember_window_size = true
+
+  -- inputs
+
+  -- allow using meta key
+  vim.g.neovide_input_use_logo = true
+
+  -- cursors
+  vim.g.neovide_cursor_antialiasing = false
   -- this is in seconds, but this is slow, so disable it untile this it fixed
+  -- vim.g.neovide_cursor_animation_length = 0.1
   vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_cursor_tail_length = 0.2
+  vim.g.neovide_cursor_tail_length = 0.1
+  vim.g.neovide_cursor_unfocused_outline_width=0.125
+  -- vim.g.neovide_cursor_vfx_mode = 'pixiedust'
 end
 
 return M
