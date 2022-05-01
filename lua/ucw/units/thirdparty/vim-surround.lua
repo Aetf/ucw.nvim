eb872aea972ae9af20ef37f61b38e31fa4e57926
Aetf <aetf@unlimited-code.works>
2022-05-01 01:08:15 -0400

    vim-surround for latex command

--- lua/ucw/units/thirdparty/vim-surround.lua
+++ lua/ucw/units/thirdparty/vim-surround.lua
@@ -16,6 +16,8 @@ M.activation = {
 
 function M.setup()
   vim.g.surround_no_insert_mappings = 1
+  -- latex command, similar to function
+  vim.g['surround_' .. vim.fn.char2nr('c')] = "\\\1command\1{\r}"
 end
 
 return M
