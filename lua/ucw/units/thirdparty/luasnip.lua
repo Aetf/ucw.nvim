33a816474758c324ce191eaaece8e4be1ac699ef
Aetf <aetf@unlimited-code.works>
2022-06-18 18:34:24 -0700

    Disable diagnostic while expanding luasnip snippets

--- lua/ucw/units/thirdparty/luasnip.lua
+++ lua/ucw/units/thirdparty/luasnip.lua
@@ -14,8 +14,24 @@ M.activation = {
   }
 }
 
+local au = require('au')
+
 function M.config()
   require("luasnip.loaders.from_vscode").load()
+  local luaship = require("luasnip")
+  -- disable diagnostic when in snippet
+  au.group('luaship-expand', {
+    { 'ModeChanged', '*:s"', function()
+      if luaship.in_snippet() then
+        return vim.diagnostic.disable()
+      end
+    end },
+    { 'ModeChanged', '[is]:n', function()
+      if luaship.in_snippet() then
+        return vim.diagnostic.enable()
+      end
+    end }
+  })
 end
 
 return M
