c38266fdf6d51bc330efdf9438429d0982b30392
Aetf <aetf@unlimited-code.works>
2022-09-15 23:43:03 -0700

    Remove debug logging

--- lua/ucw/lsp/lang/rust_analyzer.lua
+++ lua/ucw/lsp/lang/rust_analyzer.lua
@@ -2,7 +2,6 @@ local utils = require('ucw.utils')
 local M = {}
 
 function M.on_server_setup(opts)
-  vim.pretty_print(opts)
   utils.prop_set(opts, 'settings.rust-analyzer.checkOnSave.command', 'clippy')
 end
 
