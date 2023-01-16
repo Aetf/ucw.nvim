2e5ffa691d4cebb87160ea0b1c82ec44d9aa6605
Aetf <aetf@unlimited-code.works>
2023-01-16 15:39:59 -0800

    Update structlog to latest API and properly use nvim-notify

--- lua/ucw/units/thirdparty/nvim-notify.lua
+++ lua/ucw/units/thirdparty/nvim-notify.lua
@@ -18,6 +18,10 @@ function M.config ()
     min_width = 30,
   }
   vim.notify = notify
+
+  -- trigger a notify loaded autocmd, such that any pending notify calls before
+  -- nvim-notify is loaded can be displayed (e.g. in structlog)
+  vim.api.nvim_exec_autocmds('User', { pattern = 'NotifyLoaded' })
 end
 
 return M
--- lua/ucw/units/thirdparty/structlog.lua
+++ lua/ucw/units/thirdparty/structlog.lua
@@ -11,58 +11,77 @@ M.activation = {
   }
 }
 
+local pending_notify_calls = {}
+
+-- lazy load nvim notify to decouple nvim-notify from this
+local function lazy_nvim_notify(...)
+  local args = {...}
+  local ok, nvim_notify = pcall(require, 'notify')
+  if not ok then
+    -- store in pending calls, they will be displayed in NotifyLoaded autocmd
+    table.insert(pending_notify_calls, args)
+    return
+  end
+
+  nvim_notify(unpack(args))
+end
+
+local function flush_pending_notify()
+  local nvim_notify = require('notify')
+  for idx, args in ipairs(pending_notify_calls) do
+    nvim_notify(unpack(args))
+  end
+end
+
 function M.config()
   local log = require('structlog')
   log.configure({
     ucw = {
-      sinks = {
-        log.sinks.Console(
-          log.level.INFO,
-          {
-            processors = {
-              log.processors.Namer(),
-              log.processors.StackWriter({ "line", "file" }, { max_parents = 0, stack_level = 0 }),
-              log.processors.Timestamper("%H:%M:%S"),
-            },
-            formatter = log.formatters.FormatColorizer( --
-              "%s [%s] %s: %-30s",
-              { "timestamp", "level", "logger_name", "msg" },
-              { level = log.formatters.FormatColorizer.color_level() }
-            ),
-          }
-        ),
-        --[[
-        log.sinks.NvimNotify(
-          log.level.WARN,
-          {
-            processors = {
-              log.processors.Namer(),
-            },
-            formatter = log.formatters.Format( --
-              "%s",
-              { "msg" },
-              { blacklist = { "level", "logger_name" } }
-            ),
-            params_map = { title = "logger_name" },
-          }),
-        --]]
-        log.sinks.File(
-          log.level.TRACE,
-          "./test.log",
-          {
-            processors = {
-              log.processors.Namer(),
-              log.processors.StackWriter({ "line", "file" }, { max_parents = 3 }),
-              log.processors.Timestamper("%H:%M:%S"),
-            },
-            formatter = log.formatters.Format( --
-              "%s [%s] %s: %-30s",
-              { "timestamp", "level", "logger_name", "msg" }
-            ),
-          }
-        ),
-      },
+      pipelines = {
+        {
+          level = log.level.INFO,
+          processors = {
+            log.processors.StackWriter({ "line", "file" }, { max_parents = 0, stack_level = 0 }),
+            log.processors.Timestamper("%H:%M:%S"),
+          },
+          formatter = log.formatters.FormatColorizer(
+            "%s [%s] %s: %-30s",
+            { "timestamp", "level", "logger_name", "msg" },
+            { level = log.formatters.FormatColorizer.color_level() }
+          ),
+          sink = log.sinks.Console(),
+        },
+        {
+          level = log.level.WARN,
+          processors = {},
+          formatter = log.formatters.Format(
+            "%s",
+            { "msg" },
+            { blacklist = { "level", "logger_name" } }
+          ),
+          sink =  log.sinks.NvimNotify(lazy_nvim_notify),
+        },
+        {
+          level = log.level.TRACE,
+          processors = {
+            log.processors.StackWriter({ "line", "file" }, { max_parents = 3 }),
+            log.processors.Timestamper("%H:%M:%S"),
+          },
+          formatter = log.formatters.Format(
+            "%s [%s] %s: %-30s",
+            { "timestamp", "level", "logger_name", "msg" }
+          ),
+          sink = log.sinks.File("./test.log"),
+        },
+      }
     },
+  })
+
+  -- flush pending notify calls after it is loaded
+  local structlog_group = vim.api.nvim_create_augroup('StructLogNotifyLoaded', { clear = true })
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'NotifyLoaded',
+    callback = flush_pending_notify,
   })
 end
 
