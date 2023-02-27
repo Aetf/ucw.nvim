e0eb02fd8ed79242a7b78fb31a4af68373baaa99
Aetf <aetf@unlimited-code.works>
2023-02-27 00:40:30 -0800

    Move away from deprecated update_capabilities in cmp-nvim-lsp

--- lua/ucw/units/thirdparty/cmp-nvim-lsp.lua
+++ lua/ucw/units/thirdparty/cmp-nvim-lsp.lua
@@ -21,7 +21,7 @@ M.activation = {
 function M.config()
   require('ucw.lsp').register_on_server_setup('.*', function(opts)
     local caps = opts.capabilities or vim.lsp.protocol.make_client_capabilities()
-    opts.capabilities = require('cmp_nvim_lsp').update_capabilities(caps)
+    opts.capabilities = vim.tbl_deep_extend('force', caps, require('cmp_nvim_lsp').default_capabilities())
   end)
 end
 
